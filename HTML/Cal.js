9b747da9084bb4d62489a1ec9e38d9db11b7ded3
surya <20bq1a05j3@vvit.net>
2022-08-27 08:58:39 +0530

    cal

---
+++ HTML/Cal.js
@@ -0,0 +1,9 @@
+function calculate(){
+    var p=document.getElementById("result").value;
+    var q=eval(p);
+    document.getElementById("result").value=q;
+    document.getElementById("result").disabled=true;
+}
+function display(){
+    
+}
